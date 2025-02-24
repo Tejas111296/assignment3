e552f257847616a3bc6ff6895eaa0b19fb354e83
Tejas111296 <tejas.tondase@dcmail.ca>
2025-02-23 21:57:06 -0500

    Initial commit with CI/CD workflow

---
+++ tests/test_area.py
@@ -0,0 +1,18 @@
+import pytest  
+from src.area import calculate_area_square  
+  
+def test_calculate_area_square_negative():  
+    with pytest.raises(TypeError):  
+        calculate_area_square(-2)  
+  
+def test_calculate_area_square_string():  
+    with pytest.raises(TypeError):  
+        calculate_area_square("2")  
+  
+def test_calculate_area_square_list():  
+    with pytest.raises(TypeError):  
+        calculate_area_square([2])
+        
+def test_calculate_area_square_valid():
+    assert calculate_area_square(14) == 196  # 14 * 14 = 196
+
